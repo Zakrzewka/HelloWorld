bff23954f23656878e5ea076a455620211ca6d77
Mateusz <m.buczkowski@is-wireless.com>
2012-06-25 15:38:05 +0200

    Init

---
+++ HW/Source/HW.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HW.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
